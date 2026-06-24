89a0e7f32a650a833ba31ba3374efc8868a11f6c
SavitriPun <119899136+punSavitri@users.noreply.github.com>
2026-06-24 01:43:23 +0100

    added project-input component to render form content on template using OOP structure

---
+++ src/components/project-input.ts
@@ -0,0 +1,41 @@
+//handles to render form element on the page
+export class ProjectInput{
+
+    templateElement:HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    //form input fields
+    titleInputElement: HTMLInputElement;
+    descriptionElement: HTMLTextAreaElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        //selecting the <Template> element containing the form structure
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        //the main div with id="app" where form will be inserted using TypeScript
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        //import the template content into the DOM
+        const importNode = document.importNode(this.templateElement.content, true);
+        //extract the form element from the imported template
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        //added id for styling 
+        this.element.id = 'user-input';
+
+        //connect form fields to class properties for easy access
+        this.titleInputElement = document.querySelector('#title') as HTMLInputElement;
+        this.descriptionElement = document.querySelector('#description') as HTMLTextAreaElement;
+        this.peopleInputElement = document.querySelector('#people') as HTMLInputElement;
+
+        //call attach method to render form element
+        this.attach();
+    }
+
+    //method to render form element at the top of the host element
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    } 
+
+    
+}
