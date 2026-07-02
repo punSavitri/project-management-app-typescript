e1d9fb09a2e823030a070fb44f7fc982fdfc23e1
SavitriPun <119899136+punSavitri@users.noreply.github.com>
2026-07-02 23:45:09 +0100

    fix: prevent duplicate rendering by filtering projects and clearing list before update

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,4 +1,4 @@
-import { Project } from "../models/project.js";
+import { Project, ProjectStatus } from "../models/project.js";
 import { projectState } from "../state/project-state.js";
 
 //this class responsible to render the project list based on template
@@ -36,7 +36,14 @@ export class ProjectList{
 
         //event listener to assigned new projects
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+                }
+             )
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         })
         
@@ -48,6 +55,7 @@ export class ProjectList{
     }
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
