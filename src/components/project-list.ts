ac25e29bfa7129c7d5b264a58e2985519933f627
SavitriPun <119899136+punSavitri@users.noreply.github.com>
2026-07-14 01:27:48 +0100

    import  ProjectItem components to render list of single project item

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,6 +1,7 @@
 import { Project, ProjectStatus } from "../models/project.js";
 import { projectState } from "../state/project-state.js";
 import { Component } from "./base-component.js";
+import { ProjectItem } from "./project-item.js";
 
 //this class responsible to render the project list based on template
 export class ProjectList extends Component<HTMLDivElement, HTMLElement>{      
@@ -19,17 +20,7 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         this.renderContent();
 
     }
-    private renderProjects() {
-        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
-        //clear list item before to append new project
-        listEl.innerHTML = '';
-        for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
-        }
-    }    
-     configure() {
+    configure() {
         //event listener to assigned new projects
         projectState.addListener((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
@@ -44,10 +35,18 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement>{
         })
      }
     //the method updates the internal content of the component(title + list ID )
-     renderContent() {
-        
+     renderContent() {        
         const listId = `${this.type}-projects-list`;
         this.element.querySelector('ul')!.id = listId;         
         this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
     }
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        //clear list item before to append new project
+        listEl.innerHTML = '';
+        for (const prjItem of this.assignedProjects) {
+             new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
+        }
+    }    
+     
 }
