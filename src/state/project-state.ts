f072ceeebc997d6a6495ccbcdb6b6205fa264b77
SavitriPun <119899136+punSavitri@users.noreply.github.com>
2026-07-23 00:34:22 +0100

    created moveProject() to update state and render project item

--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -41,6 +41,18 @@ export class ProjectState{
             ProjectStatus.Active   //setting the project status Active by default
         )
         this.projects.push(newProject);
+        this.updateListeners();
+        
+    }
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
